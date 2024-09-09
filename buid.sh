f4cdcb8705d53dabb782f46f5339bf8e767a7b7a
Albin Tom Kuruvilla <albinkuruvilla1926@gmail.com>
2024-09-09 21:19:08 +0530

    Create buid.sh

---
+++ buid.sh
@@ -0,0 +1,9 @@
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi
