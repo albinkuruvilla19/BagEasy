6c0336e952347579216c88e65f5a190a26895da7
Albin Tom Kuruvilla <albinkuruvilla1926@gmail.com>
2024-09-09 21:59:06 +0530

    Update settings.py

--- bageasy/settings.py
+++ bageasy/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^4f)cvseqcsf5^rhpukfiy=fc8=$zz111y1ycr4d^u!wuli1!='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
